52a4ca96058bca497dba3093d965d8a3dd368104
Yvan Brunel <8brunel@gmail.com>
2021-07-12 13:52:05 +0100

    Added Login/LogOut/Profile Management. NO CSRF Token

---
+++ accounts/forms.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class EditProfileForm(UserChangeForm):
+    password = forms.CharField(label="", widget=forms.TextInput(attrs={'type':'hidden',}))
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name','email','password')
+       # TO EXCLUDE FIELDS exclude = ('groups','is_staff','is_superuser', 'is_active', 'groups','user_permissions','date_joined','password','last_login')
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Email Address'}))
+    first_name = forms.CharField( label="",max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=100,  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name','last_name','email','password1','password2',)
+    
+    #to stylize the form inputs with bootstrap
+    def __init__(self,*args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<span class="form-text"><small><ul> <li>Your password can’t be too similar to your other personal information.</li> <li>Your password must contain at least 8 characters.</li> <li>Your password can’t be a commonly used password.</li> <li>Your password can’t be entirely numeric.</li></ul></small></span>'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text"><small>Enter the same password as before, for verification.</small></span>'
+
+        
